272e5bd4188b1d32ad0fe6b784ec2842e1f46d98
Idress BENCHELLALI <ibenchellali@amiltone.com>
2023-04-15 21:07:11 +0200

    Initial Commit

---
+++ memoryApp/memoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoryAppApp.swift
+//  memoryApp
+//
+//  Created by Idress BENCHELLALI on 15/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct memoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
